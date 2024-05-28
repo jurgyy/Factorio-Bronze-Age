64fa1d486d23ed2fdbf8616dd62204cf5243f3d5
Jurgen <jurgen.baas@outlook.com>
2024-05-28 21:08:44 +0200

    Added special case for curved-rail. Maybe need a more generic solution

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,16 +1,24 @@
 for type in pairs(defines.prototypes.entity) do
     for _, prototype in pairs(data.raw[type]) do
         if prototype.minable then
-            local recipe = data.raw.recipe[prototype.name]
+            local recipe = data.raw.recipe[prototype.minable.result]
             if recipe and (recipe.ingredients or recipe.normal) then
                 local ingredients = (recipe.ingredients or recipe.normal.ingredients)
+                local multiplier = 1
+                if prototype.name == "curved-rail" then
+                    multiplier = 4
+                end
                 local results = {}
+                if recipe.result_count and recipe.result_count > 1 then
+                    log("Modifying the result_count for " .. recipe.name .. " from " .. recipe.result_count .. " to 1")
+                    recipe.result_count = 1
+                end
                 for _, ingredient in ipairs(ingredients) do
                     if ingredient.type ~= "fluid" then
                         table.insert(results, {
                             type = "item",
                             name = ingredient[1],
-                            amount = ingredient[2]
+                            amount = ingredient[2] * multiplier
                         })
                     else
                         log(prototype.name .. " Skipping fluid ingredient")
@@ -24,6 +32,5 @@ for type in pairs(defines.prototypes.entity) do
                 log("No recipe for " .. prototype.name)
             end
         end
-        log(prototype.name)
     end
 end
