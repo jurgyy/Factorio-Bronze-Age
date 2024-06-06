73bfc9e773e607d00fa68cb7651cdae54b8d9cc7
Jurgen <jurgen.baas@outlook.com>
2024-06-06 16:29:15 +0200

    Fixed type annotations not working properly

--- PathfindingCollection.lua
+++ PathfindingCollection.lua
@@ -1,5 +1,14 @@
 local util = require("ba-util")
 
+---@class PathfindingCollection
+---@field surface_index integer Surface index for the pathfinder
+---@field unit_prototype LuaEntityPrototype prototype of the entity that has to walk the path
+---@field goal_positions MapPosition[] Array of all positions that are valid for reaching the goal
+---@field goal_entity LuaEntity Entity that requests the item
+---@field to_be_checked table<integer, Path[]> Table that maps a start entity unit_number to an array of all possible Paths between it and the goal
+---@field item_name string Name of the item requested
+---@field total_amount integer? Total minimum amount of weight requested for if multiple paths. Defaults to 1
+---@field private success_amount integer Counter to keep track of the amount of weight already passed
 local pathfinding_collection = {}
 pathfinding_collection.__index = pathfinding_collection
 
@@ -12,21 +21,7 @@ pathfinding_collection.__index = pathfinding_collection
 ---@field start_entity LuaEntity? Optional entity that is required to be valid at the start
 ---@field collection PathfindingCollection Parent collection containing this path
 
----@class PathfindingCollection
----@field surface_index integer Surface index for the pathfinder
----@field unit_prototype LuaEntityPrototype prototype of the entity that has to walk the path
----@field goal_positions MapPosition[] Array of all positions that are valid for reaching the goal
----@field goal_entity LuaEntity Entity that requests the item
----@field to_be_checked table<integer, Path[]> Table that maps a start entity unit_number to an array of all possible Paths between it and the goal
----@field item_name string Name of the item requested
----@field total_amount integer? Total minimum amount of weight requested for if multiple paths. Defaults to 1
----@field private success_amount integer Counter to keep track of the amount of weight already passed
----@field add_path fun(Path)
----@field add_paths_from_entity fun(LuaEntity, integer)
----@field request_path fun(Path)
----@field path_found_finished fun(Path)
----@field request_next fun()
---@field private successes integer[]
+
 
 ---Returns a new PathfindingCollection object
 ---@param surface_index integer
@@ -36,7 +31,6 @@ pathfinding_collection.__index = pathfinding_collection
 ---@param goal_entity LuaEntity Target entity for the pathfinding
 ---@return PathfindingCollection?
 function pathfinding_collection.new(surface_index, item_name, total_amount, unit_prototype, goal_entity)
-    --[[@type PathfindingCollection]]
     local self = setmetatable({}, pathfinding_collection)
     local goal_positions = util.get_path_sets_around(goal_entity.surface, goal_entity.bounding_box)
     if not goal_positions or not goal_positions[1] then
