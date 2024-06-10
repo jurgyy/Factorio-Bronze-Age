6739b2df3c0eb895e0ce0bb09720464b18535d20
Jurgen <jurgen.baas@outlook.com>
2024-06-10 17:00:45 +0200

    Added bricks recipe

---
+++ data/bricks/bricks-recipe.lua
@@ -0,0 +1,13 @@
+return {
+    type = "recipe",
+    name = "bricks",
+    localised_name = {"bricks"},
+    enabled = false,
+    ingredients =
+    {
+      {"wood", 10},
+      {"clay", 10}
+    },
+    energy_required = 5,
+    result = "bricks"
+}
