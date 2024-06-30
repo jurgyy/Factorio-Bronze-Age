54c3a62171812284ca496bfeba4838b3b1473b4e
Jurgen <jurgen.baas@outlook.com>
2024-06-30 10:33:53 +0200

    Added simple-furnace-entity to data:extend

--- data/simple-furnace/data.lua
+++ data/simple-furnace/data.lua
@@ -1,7 +1,8 @@
 local data_util = require("__bronze-age__/data/data-util")
 local root = data_util.data_root .. "simple-furnace/"
 
-local item = require(root .. "simple-furnace-item")
-local recipe = require(root .. "simple-furnace-recipe")
-
-data:extend{item, recipe}
+data:extend{
+    require(root .. "simple-furnace-item"),
+    require(root .. "simple-furnace-recipe"),
+    require(root .. "simple-furnace-entity")
+}
