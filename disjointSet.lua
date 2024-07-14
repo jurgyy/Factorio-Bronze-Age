d81c2eb02a44985df74ff53ce6f7e74e65576322
Jurgen <jurgen.baas@outlook.com>
2024-07-14 10:01:03 +0200

    Added inefficent support for removing nodes by storing the root parent's childeren and recalculating the set for them

--- disjointSet.lua
+++ disjointSet.lua
@@ -5,8 +5,9 @@ util = require("ba-util")
 ---@field y integer
 
 --- @class DisjointSet
---- @field parent table<integer, table<integer, DisjointSetIndex>>
---- @field rank table<integer, table<integer, integer>>
+--- @field parent table<integer, table<integer, DisjointSetIndex>> table[x_self][y_self] -> {x: x_parent, y: y_parent}
+--- @field rank table<integer, table<integer, integer>> table[x_self][y_self] -> rank
+--- @field children table<integer, table<integer, DisjointSetIndex[]?>> table[x_self][y_self] -> child_index[]?
 local DisjointSet = {}
 DisjointSet.__index = DisjointSet
 script.register_metatable("disjoint_set", DisjointSet)
@@ -15,12 +16,20 @@ script.register_metatable("disjoint_set", DisjointSet)
 --- @field tile? LuaTile
 --- @field position? MapPosition
 
+---Are the two positions equal?
+---@param pos1 DisjointSetIndex?
+---@param pos2 DisjointSetIndex?
+local function equal(pos1, pos2)
+    return pos1 == pos2 or (pos1 and pos2 and pos1.x == pos2.x and pos1.y == pos2.y)
+end
+
 ---Creates a new DisjointSet
 ---@return DisjointSet
 function DisjointSet.new()
     local self = setmetatable({}, DisjointSet)
     self.parent = {}
     self.rank = {}
+    self.children = {}
     return self
 end
 
@@ -46,10 +55,45 @@ end
 
 ---@param position DisjointSetIndex The index position
 ---@param parent DisjointSetIndex? The parent
-function DisjointSet:set_parent(position, parent)
+---@param add_child boolean?
+function DisjointSet:set_parent(position, parent, add_child)
+    add_child = add_child or add_child == nil -- nil -> true
     local parent_x = self.parent[position.x] or {}
     parent_x[position.y] = parent
     self.parent[position.x] = parent_x
+
+    if self.children[position.x] and self.children[position.x][position.y] then
+        for _, child in pairs(self.children[position.x][position.y]) do
+            self:set_parent(child, parent)
+        end
+    end
+
+    if add_child and parent and not equal(position, parent) then
+        self:add_as_child(position, parent --[[@as DisjointSetIndex]])
+    end
+end
+
+---@param child DisjointSetIndex
+---@param parent DisjointSetIndex
+function DisjointSet:add_as_child(child, parent)
+    if not self.children[parent.x] then
+        self.children[parent.x] = {}
+    end
+    
+    if not self.children[parent.x][parent.y] then
+        self.children[parent.x][parent.y] = {}
+    end
+
+    table.insert(self.children[parent.x][parent.y], child)
+    
+    if not self.children[child.x] then
+        return
+    end
+
+    self.children[child.x][child.y] = nil
+    if not next(self.children[child.x]) then
+        self.children[child.x] = nil
+    end
 end
 
 ---@param position DisjointSetIndex The index position
@@ -68,40 +112,31 @@ function DisjointSet:set_rank(position, value)
     self.rank[position.x] = x
 end
 
----Are the two positions equal?
----@param pos1 DisjointSetIndex?
----@param pos2 DisjointSetIndex?
-local function equal(pos1, pos2)
-    return pos1 == pos2 or (pos1 and pos2 and pos1.x == pos2.x and pos1.y == pos2.y)
-end
 
 ---Is the index in the disjointed set? Returns the index of itself if it is alone.
 ---The index of its parent if in a group or nil if it is not in the entire set.
 ---@param index DisjointSetIndex?
 ---@return DisjointSetIndex?
 function DisjointSet:find(index)
-    local parent = self:get_parent(index)
-    if not equal(parent, index) and index then
-        self:set_parent(index, self:find(parent))
-    end
     return self:get_parent(index)
 end
 
 ---Adds a connection between two indices
 ---@param index1 DisjointSetIndex
 ---@param index2 DisjointSetIndex
-function DisjointSet:union(index1, index2)
+---@param add_child boolean?
+function DisjointSet:union(index1, index2, add_child)
     local root1 = self:find(index1)
     local root2 = self:find(index2)
     if root1 and root2 and not equal(root1, root2) then
         local rank1 = self:get_rank(root1)
         local rank2 = self:get_rank(root2)
         if rank1 > rank2 then
-            self:set_parent(root2, root1)
+            self:set_parent(root2, root1, add_child)
         elseif rank1 < rank2 then
-            self:set_parent(root1, root2)
+            self:set_parent(root1, root2, add_child)
         else
-            self:set_parent(root2, root1)
+            self:set_parent(root2, root1, add_child)
             self:set_rank(root1, rank1 + 1)
         end
     end
@@ -116,16 +151,48 @@ function DisjointSet:add(tile_or_position)
         return
     end
     position = self:get_index(position)
-    
-    if self:get_parent(position) == nil then
-        self:set_parent(position, position)
+    self:add_index(position)
+end
+
+function DisjointSet:add_index(index)
+    if self:get_parent(index) == nil then
+        self:set_parent(index, index)
         -- We do not initialize the rank since the getter already returns 0 if it's not stored
     end
 
-    self:union(position, {x = position.x - 1, y = position.y})
-    self:union(position, {x = position.x + 1, y = position.y})
-    self:union(position, {x = position.x, y = position.y - 1})
-    self:union(position, {x = position.x, y = position.y + 1})
+    self:union(index, {x = index.x - 1, y = index.y}, false)
+    self:union(index, {x = index.x + 1, y = index.y}, false)
+    self:union(index, {x = index.x, y = index.y - 1}, false)
+    self:union(index, {x = index.x, y = index.y + 1}, false)
+end
+
+---Removes a possition from the disjoint set and then recalculates the disjoint set for all childeren of the root parent. Extremely inefficient.
+---@param tile_or_position TileOrPosition
+function DisjointSet:remove(tile_or_position)
+    local position = tile_or_position.position or tile_or_position.tile.position
+    if not position then
+        util.print("Cannot add value: no tile or position given")
+        return
+    end
+    position = self:get_index(position)
+    
+    local parent = self:get_parent(position)
+    local children = parent and self.children[parent.x] and self.children[parent.x][parent.y]
+    self.parent[position.x][position.y] = nil
+    if children then
+
+        for i, child in pairs(children) do
+            if equal(child, position) then
+                children[i] = nil
+            end
+
+            self.parent[child.x][child.y] = nil
+        end
+
+        for _, child in pairs(children) do
+            self:add_index(child)
+        end
+    end
 end
 
 ---Are two positions connect
