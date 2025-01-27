276fc7da6ede9c22597556aabb79058d1ec48bff
Jurgen <jurgen.baas@outlook.com>
2025-01-27 16:22:19 +0100

    Removed whitespace from lines with just whitespace

--- graphics/placeholder/construction/rotate.py
+++ graphics/placeholder/construction/rotate.py
@@ -18,15 +18,15 @@ for file_name in files:
     if file_name.endswith('.png'):
         image_path = os.path.join(input_directory, file_name)
         img = Image.open(image_path)
-        
+
         img.save(os.path.join(output_directories['N'], file_name))
-        
+
         img_90 = img.rotate(-90, expand=True)
         img_90.save(os.path.join(output_directories['E'], file_name))
-        
+
         img_180 = img_90.rotate(-90, expand=True)
         img_180.save(os.path.join(output_directories['S'], file_name))
-        
+
         img_270 = img_180.rotate(-90, expand=True)
         img_270.save(os.path.join(output_directories['W'], file_name))
 
